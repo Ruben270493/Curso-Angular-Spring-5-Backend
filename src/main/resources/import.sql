d9c9467ec9c4c6398c60875e228e1591d8e9192b
Ruben <ruben270493@gmail.com>
2020-02-13 19:46:29 +0100

    Datos de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Tabla Clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Rubén","Fernández de Castro Cambrón", "ruben@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Trang","Bui", "trang@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Fernando","Fernández de Castro Cambrón", "fernando@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Laura","Fernández de Castro Cambrón", "laura@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Mariangeles","Perez", "mariangeles@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Manolo","Perez", "manolo@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Juan","Villazan", "juan@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Jose","Portugal", "jose@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Mari","Galicia", "mari@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Laura","Cuba", "laurac@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Carlos","Lopez", "carlos@spring.com", "2020-02-13");
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ("Sergio","gonzalez", "sergio@spring.com", "2020-02-13");
