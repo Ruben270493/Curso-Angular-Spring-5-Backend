519f80db402e5800fc35dd073d29b11cdc573a01
Ruben <ruben270493@gmail.com>
2020-02-22 16:40:21 +0100

    EnableResourceServer

--- src/main/java/com/rfernandez/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/rfernandez/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -3,9 +3,11 @@ package com.rfernandez.springboot.backend.apirest.auth;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -21,3 +21,14 @@ INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES ("Ma
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES ("Laura","Cuba", "laurac@spring.com", "2020-02-13", 2);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES ("Carlos","Lopez", "carlos@spring.com", "2020-02-13", 4);
 INSERT INTO clientes (nombre, apellido, email, create_at, region_id) VALUES ("Sergio","gonzalez", "sergio@spring.com", "2020-02-13", 8);
+
+/* Tablas Usuarios y Roles */
+INSERT INTO usuarios (username, password, enabled) VALUES ('ruben', '$2a$10$mQ8.mN/mY/eoupnHN5AUWOVM5pq98kzGCfqFIWHXpt/qO6KM3lpfi', 1);
+INSERT INTO usuarios (username, password, enabled) VALUES ('admin', '$2a$10$AaY8t04W0HewyGDkYszdVe1RrmpMM9z7TpULsbcUS3aHwQ7iJ1wbG', 1);
+
+INSERT INTO roles (nombre) VALUES ('ROLE_USER');
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuarios_id, roles_id) VALUES (1, 1);
+INSERT INTO usuarios_roles (usuarios_id, roles_id) VALUES (2, 2);
+INSERT INTO usuarios_roles (usuarios_id, roles_id) VALUES (2, 1);
